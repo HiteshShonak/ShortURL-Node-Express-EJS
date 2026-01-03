116581d9f3f1dc306019bcc36f3e1d97b78122c0
HiteshShonak <hiteshshonak1@gmail.com>
2026-01-03 11:29:53 +0530

    Added Mongoose schemas for User and URL data

---
+++ models/url.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+shortId: { type: String, required: true, unique: true },
+redirectUrl: { type: String, required: true },
+visitHistory: [{timestamp: {type: Number}, 
+                device: { type: String }, 
+                ip: { type: String },
+                location: { type: String },
+                referrer: { type: String },
+                region: { type: String },
+                latitude: { type: Number },
+                longitude: { type: Number },
+               }],
+createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
+},
+{ timestamps: true }
+);
+
+const URL = mongoose.model('Url', urlSchema);
+
+module.exports = URL;
