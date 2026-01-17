3cfc1fe1504d32b83a5f68fef249d78379fa787b
HiteshShonak <hiteshshonak1@gmail.com>
2026-01-17 14:17:00 +0530

    setup cron job

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const { connectToDatabase } = require('./connection');
 const cookieParser = require('cookie-parser');
-const {checkAuthentication, restrictTo} = require('./middleware/auth');
+const { checkAuthentication, restrictTo } = require('./middleware/auth');
 
 // 1. Import Routes
 const urlRoute = require('./routes/url.js');
@@ -21,21 +21,24 @@ connectToDatabase(mongoURL);
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-
 // 4. Middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // This allows parsing form data
+app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
+
+app.get('/api/ping', (req, res) => {
+    console.log("🔔 Keep-Alive Ping Received");
+    res.status(200).send("Pong");
+});
+
 app.use(checkAuthentication);
 
-
-
 // 5. Routes
-app.use('/user', userRoute); // Handles User Signup (e.g., /user/signup)
-app.use('/url', urlRoute); // Handles generating and redirecting (e.g., /url/abc)
-app.use('/', staticRoute); // Handles the Home Page UI
+app.use('/user', userRoute); 
+app.use('/url', urlRoute); 
+app.use('/', staticRoute); 
 
 app.listen(port, () => {
-  console.log(`Server Started`);
+  console.log(`Server Started on port ${port}`);
 });
